98493e659db178673ae2ebf36c3272bdefde9112
Kyle Devereaux <kdevero@mit.edu>
2025-08-05 15:59:10 +0200

    updated running scripts, pull from latest mainOO changes, incorperated

--- SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/ProcessSingleFile-OO-xrd.sh
+++ SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/ProcessSingleFile-OO-xrd.sh
@@ -3,17 +3,13 @@
 FILEPATH=${1}
 COUNTER=${2}
 OUTPUT=${3}
-DOGENLEVEL=${4}
-ISDATA=${5}
-SAMPLETYPE=${6}
-SAVETRIGGERBITS=${7}
-DEBUGMODE=${8}
-INCLUDEPPSANDFSC=${9}
-INCLUDEPF=${10}
-SERVER=${11}
-MAXCORES=${12}
-
-EFFPATH=${ProjectBase}/CommonCode/root/
+ISDATA=${4}
+APPLYTRIGGERREJECTION=${5}
+APPLYEVENTREJECTION=${6}
+APPLYTRACKREJECTION=${7}
+SAMPLETYPE=${8}
+SERVER=${9}
+MAXCORES=${10}
 
 mkdir -p "${OUTPUT}/temp_inputs/"
 FILE="${OUTPUT}/temp_inputs/job_${COUNTER}.root"
@@ -23,25 +19,18 @@ wait
 
 ./Execute --Input "$FILE" \
    --Output ${OUTPUT}/output_${COUNTER}.root \
-   --Year 2025 \
    --IsData $ISDATA \
-   --IsPP false \
+   --CollisionSystem OO \
    --Fraction 1.0 \
-   --ApplyTriggerRejection true \
-   --ApplyEventRejection false \
-   --ApplyTrackRejection true \
+   --ApplyTriggerRejection $APPLYTRIGGERREJECTION \
+   --ApplyEventRejection $APPLYEVENTREJECTION \
+   --ApplyTrackRejection $APPLYTRACKREJECTION \
    --PFTree particleFlowAnalyser/pftree \
    --sampleType $SAMPLETYPE \
-   --DebugMode $DEBUGMODE \
-   --includeFSCandPPSMode $INCLUDEPPSANDFSC \
-   --includePFMode $INCLUDEPF \
-   --saveTriggerBitsMode $SAVETRIGGERBITS \
-   --TrackEfficiencyPath $EFFPATH \
-   --MakeEventWeight true \
-   --EvtSelCorrectionFile "${EFFPATH}OORAA_MULT_EFFICIENCY_HIJING_HF13AND.root,${EFFPATH}OORAA_MULT_EFFICIENCY_HIJING_HF19AND.root,${EFFPATH}OORAA_MULT_EFFICIENCY_HIJING_HF10AND.root" \
-   --MC_ReweightFile "${EFFPATH}OORAA_MC_Reweight_HIJING_HF13AND.root" \
-   --Species_ReweightFile "${EFFPATH}ParticleSpeciesCorrectionFactorsOO.root" \
-   --HideProgressBar false
+   --DebugMode false \
+   --includeL1EMU false \
+   --CorrectionPath ${ProjectBase}/CommonCode/root/ \
+   --HideProgressBar true
 wait
 
 sleep 0.2
